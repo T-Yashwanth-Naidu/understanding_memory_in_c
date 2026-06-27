94e951b3a1bcf83499cd926d94a4c75b1d181849
T-Yashwanth-Naidu <yashwanthtikkisetty@gmail.com>
2026-06-27 00:08:28 -0400

    initial code strace

--- initial_program.c
+++ initial_program.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
 
 int main(){
 
    // Assign some memory using malloc
    char *store_data = malloc(11);
-    
+   char *source = "0123456789";
    if(!store_data)
    {
        return 1;
    }
 
-   memcpy(store_data, "0123456789",11);
+   for(int i=0;i<11;i++)
+   {
+       store_data[i] = source[i];
+   }
+
  
    // Malloc returns the address that is in the heap region for that data which is assigned which is created on heap. 
    printf("Data stored: %s at memory location: %p\n", store_data, (void*)store_data);
